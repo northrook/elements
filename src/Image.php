76b9742a3ae05ec985f36e152a6ebbdfcac11dfd
Martin <hello@northrook.com>
2024-03-25 08:37:22 +0000

    mend

--- src/Image.php
+++ src/Image.php
@@ -6,6 +6,16 @@ namespace Northrook\Elements;
 
 use Northrook\Elements\Element\Asset;
 
+/**
+ * Handle both single-url and srcset images
+ *
+ * * Wrap each image in a <picture> tag
+ * * Add a <source> tag for each image
+ * * Caption
+ * * Alt
+ * * Author/copyright
+ * * Blurhash
+ */
 class Image extends Asset
 {
 
